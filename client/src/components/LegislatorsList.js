b3fc9c7586564d97d18ebab63e1701f980939ceb
Welpato <wesleyfosorio@hotmail.com>
2024-10-09 18:36:48 -0300

    Initial UI and Legislators list creation

---
+++ client/src/components/LegislatorsList.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+
+function LegislatorsList() {
+    const [legislators, setLegislators] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/legislators/votes')
+        .then(response => response.json())
+        .then(data => setLegislators(data));
+    })
+
+    const renderLegislatorsList = () => {
+        return legislators.map(legislator => {
+            return (
+                <tr key={legislator.id}>
+                    <td>{legislator.id}</td>
+                    <td>{legislator.legislator}</td>
+                    <td>{legislator.supportedBills}</td>
+                    <td>{legislator.opposedBills}</td>
+                </tr>
+            );
+        });
+    }
+
+    return (
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Legislator</th>
+                    <th>Supported bills</th>
+                    <th>Opposed bills</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderLegislatorsList()}
+            </tbody>
+        </Table>
+    );
+}
+
+export default LegislatorsList;
---
+++ client/src/components/Menu.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Menu() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">BVA</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#legislators">Legislators</Nav.Link>
+            <Nav.Link href="#bills">Bills</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Menu;
