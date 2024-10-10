3e0404b669cb080e1a4990029b4717dde53ead1a
Welpato <wesleyfosorio@hotmail.com>
2024-10-10 09:41:58 -0300

    Add error handling to simpleFetch function

--- client/src/utils/simpleFetch.js
+++ client/src/utils/simpleFetch.js
@@ -1,3 +1,6 @@
 export default function simpleFetch(url) {
-    return fetch(url).then(result => result.json());
+    return fetch(url).then(result => result.json()).catch(error => {
+        console.error(error);
+        return [];
+    });
 }
